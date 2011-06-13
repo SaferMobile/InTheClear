4e23250ba239cdba08d3d2e7e302f2f138e1d368
Nathan Freitas <nathan@freitas.net>
2011-06-13 01:38:16 -0400

    manual Shout! app and form implemented

--- projects/j2me/src/org/safermobile/clear/micro/apps/ShoutMIDlet.java
+++ projects/j2me/src/org/safermobile/clear/micro/apps/ShoutMIDlet.java
@@ -2,33 +2,27 @@
 /* See LICENSE for licensing information */
 
 package org.safermobile.clear.micro.apps;
-import java.io.IOException;
-import java.util.Date;
-
 import javax.microedition.lcdui.Alert;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.TextBox;
-import javax.microedition.lcdui.TextField;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
-import javax.microedition.rms.RecordStoreException;
 
-import org.safermobile.clear.micro.L10nConstants;
+import org.j4me.ui.UIManager;
 import org.safermobile.clear.micro.L10nResources;
-import org.safermobile.clear.micro.data.PhoneInfo;
-import org.safermobile.clear.micro.sms.SMSManager;
+import org.safermobile.clear.micro.apps.screens.ShoutManualForm;
+import org.safermobile.clear.micro.ui.ClearTheme;
 import org.safermobile.clear.micro.ui.LargeStringCanvas;
 import org.safermobile.micro.ui.DisplayManager;
 import org.safermobile.micro.utils.Logger;
 import org.safermobile.micro.utils.Preferences;
-import org.safermobile.micro.utils.StringTokenizer;
 
 // hasLocationCapability=false
 //release.build = false
-public class ShoutMIDlet extends MIDlet implements CommandListener, Runnable {
+public class ShoutMIDlet extends MIDlet implements CommandListener {
 
 	
 	private DisplayManager _manager;
@@ -36,23 +30,12 @@ public class ShoutMIDlet extends MIDlet implements CommandListener, Runnable {
 
 	private TextBox _tbMain;
 	private LargeStringCanvas _lsCanvas;
-	
-	private Command	 _cmdSend;	
-	private Command	 _cmdExit;
-	
-	//private SMSManager _smsServer;
-	//private int SMS_SERVER_PORT = 0;
-	
+	private ShoutManualForm _form;
 	
 	/*
 	 * the thread which manages the panic sending
 	 */
 	private Thread _thread;
-	
-	/*
-	 * used to cancel the panic loop
-	 */
-	private boolean _keepPanicing = false; 
 	
 	/*
 	 * stores the user data between the config app and this one
@@ -70,18 +53,11 @@ public class ShoutMIDlet extends MIDlet implements CommandListener, Runnable {
 	public ShoutMIDlet() {
 		
 		_display = Display.getDisplay(this);
-		_manager = new DisplayManager(_display);
 		
-		_cmdSend = new Command("Shout!", Command.OK,0);
-		_cmdExit = new Command("Exit", Command.EXIT,1);
-		
-		_tbMain = new TextBox(l10n.getString(L10nConstants.keys.KEY_PANIC_TITLE_MAIN), "", 500, TextField.ANY);
-		
-		_tbMain.setCommandListener(this);
-		
-		_tbMain.addCommand(_cmdSend);
-		_tbMain.addCommand(_cmdExit);
+		UIManager.init(this);
+		 UIManager.setTheme( new ClearTheme()  );
 
+		 _form = new ShoutManualForm(this);
 	}
 
 	
@@ -92,8 +68,8 @@ public class ShoutMIDlet extends MIDlet implements CommandListener, Runnable {
 	 */
 	protected void startApp() throws MIDletStateChangeException {
 		
-		showAlert("Shout!","Enter the message you want to shout to your configured friends.",_tbMain);
 
+		_form.show();
 	
 	}
 	
@@ -126,19 +102,6 @@ public class ShoutMIDlet extends MIDlet implements CommandListener, Runnable {
 	 * @see javax.microedition.midlet.MIDlet#pauseApp()
 	 */
 	protected void pauseApp() {}
-	
-	
-	private void doSecPause (int secs)
-	{
-		try { Thread.sleep(secs * 1000);}
-		catch(Exception e){}
-	}
-	
-	public void run ()
-	{
-		
-		
-	}
 	
 	
 	
---
+++ projects/j2me/src/org/safermobile/clear/micro/apps/screens/ShoutManualForm.java
@@ -0,0 +1,117 @@
+package org.safermobile.clear.micro.apps.screens;
+
+
+import javax.microedition.lcdui.Graphics;
+
+import org.j4me.ui.*;
+import org.j4me.ui.components.*;
+import org.safermobile.clear.micro.L10nConstants;
+import org.safermobile.clear.micro.L10nResources;
+import org.safermobile.clear.micro.apps.PanicConfigMIDlet;
+import org.safermobile.clear.micro.apps.ShoutMIDlet;
+import org.safermobile.clear.micro.apps.controllers.ShoutController;
+import org.safermobile.clear.micro.ui.ErrorAlert;
+
+/**
+ * Example of a <code>TextBox</code> component.
+ */
+public class ShoutManualForm
+        extends Dialog implements Runnable
+{
+        /**
+         * The previous screen.
+         */
+        private ShoutMIDlet midlet;
+        
+        /**
+         * The number box used by this example for entering phone numbers.
+         */
+        private TextBox tbPhoneNumber;        
+        private TextBox tbMessage;
+        
+    	private Label _label = new Label();
+
+		L10nResources l10n = L10nResources.getL10nResources(null);
+
+        /**
+         * Constructs a screen that shows a <code>TextBox</code> component in action.
+         * 
+         * @param previous is the screen to return to once this done.
+         */
+        public ShoutManualForm (ShoutMIDlet midlet)
+        {
+                this.midlet = midlet;
+                
+                // Set the title and menu.
+                setTitle( "Shout!" );
+                setMenuText( "Exit" ,  l10n.getString(L10nConstants.keys.KEY_MENU_SEND) );
+
+             // Center the text.
+        		_label.setHorizontalAlignment( Graphics.LEFT );
+
+        		_label.setLabel("Send a Shout! Message");
+        		
+        		// Add the label to this screen.
+        		append( _label );
+        		
+                // Add the phone number box.
+        		tbPhoneNumber = new TextBox();
+        		tbPhoneNumber.setLabel( l10n.getString(L10nConstants.keys.KEY_PANIC_LBL_PHONE_NUMBER) );
+                tbPhoneNumber.setForPhoneNumber();
+                tbPhoneNumber.setMaxSize( 20 );
+                append( tbPhoneNumber );
+                
+                // Add the phone number box.
+        		tbMessage = new TextBox();
+        		tbMessage.setLabel("Shout! Message" );        		
+                append( tbMessage );
+               
+        }
+
+        /**
+         * Takes the user to the previous screen.
+         */
+        protected void declineNotify ()
+        {
+        	midlet.notifyDestroyed();
+
+        }
+        
+        protected void acceptNotify() 
+        {		
+        	//do SMS test
+        	new Thread(this).start();
+        }
+        
+        public void run ()
+        {
+        	sendShoutMessage();
+
+        }
+        
+        private void sendShoutMessage ()
+        {
+        	String userName = "";
+        	
+        	String recip = tbPhoneNumber.getString();
+        	String msg =  tbMessage.getString();
+        	
+        	ShoutController sc = new ShoutController();
+        	
+
+        	String data = sc.buildShoutData(userName);
+        	
+        	try {
+				sc.sendSMSShout(recip, msg, data);
+				ErrorAlert eAlert = new ErrorAlert ("Success!", "Your Shout! was sent.", null, this);
+				eAlert.show();
+				
+			} catch (Exception e) {
+				
+				ErrorAlert eAlert = new ErrorAlert ("Error!", "Unable to send Shout! message. Try again.", null, this);
+				eAlert.show();
+				e.printStackTrace();
+			}
+        }
+        
+}

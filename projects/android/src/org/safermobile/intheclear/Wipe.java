186eec0f13e91eeed6f5d837fb6e4275e5d49585
Harlo Holmes <harlo.holmes@gmail.com>
2011-06-15 13:57:45 -0400

    new array adaptor class for handling wipe preferences.

--- projects/android/src/org/safermobile/intheclear/Wipe.java
+++ projects/android/src/org/safermobile/intheclear/Wipe.java
@@ -2,7 +2,8 @@ package org.safermobile.intheclear;
 
 import java.util.ArrayList;
 
-import org.safermobile.intheclear.screens.WipeSelectionForm;
+import org.safermobile.intheclear.ui.WipeArrayAdaptor;
+import org.safermobile.intheclear.ui.WipeSelector;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
@@ -10,8 +11,10 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 public class Wipe extends Activity implements OnClickListener {
@@ -19,6 +22,7 @@ public class Wipe extends Activity implements OnClickListener {
 	
 	Button wipeButton;
 	ListView checkBoxHolder;
+	ArrayList<WipeSelector> wipeSelector;
 	
 	
 	private final static String ITC = "[InTheClear:Wipe] ************************ ";
@@ -29,10 +33,17 @@ public class Wipe extends Activity implements OnClickListener {
 		setContentView(R.layout.wipe);
 		
 		_sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		boolean shouldWipeContacts = _sp.getBoolean("DefaultWipeContacts",false);
+		boolean shouldWipePhotos = _sp.getBoolean("DefaultWipePhotos", false);
+		
+		wipeSelector = new ArrayList<WipeSelector>();
+		
+		wipeSelector.add(new WipeSelector(getString(R.string.KEY_WIPE_WIPECONTACTS), 1, shouldWipeContacts));
+		wipeSelector.add(new WipeSelector(getString(R.string.KEY_WIPE_WIPEPHOTOS), 2, shouldWipePhotos));
+
+		
 		checkBoxHolder = (ListView) findViewById(R.id.checkBoxHolder);
-		
-				
-		
+		checkBoxHolder.setAdapter(new WipeArrayAdaptor(this, wipeSelector));
 		
 	}
 
